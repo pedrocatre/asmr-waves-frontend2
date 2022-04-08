fd2a6fde7f2de09f1d17168c01648471a56d1762
pedro <gil.catre@gmail.com>
2022-04-08 10:50:51 +0200

    added slider

--- src/Sound.jsx
+++ src/Sound.jsx
@@ -1,7 +1,64 @@
-export default function Sound() {
-    return (
-        <main style={{ padding: "1rem 0" }}>
-            <h2>Sound</h2>
-        </main>
-    );
+import React, {Component} from 'react';
+import Slider from 'react-rangeslider'
+import 'react-rangeslider/lib/index.css'
+import classNames from 'classnames';
+
+class Sound extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            volume: this.props.sound.volume
+        }
+    }
+
+    handleChange = volume => {
+        this.setState({
+            volume: volume
+        });
+        this.props.handleChangeVolume(this.props.index, volume);
+        // this.props.sound.sound.volume(value/100.0);
+        // this.props.sound.volume = value;
+    };
+
+    render() {
+        let isSoundPlayingClass = classNames({
+            "col": true,
+            sound: true,
+            'sound-not-active': !this.props.sound.isPlaying,
+        });
+        return (
+            <div className={isSoundPlayingClass}>
+                <div className="card shadow-sm">
+                    {/*<svg className="bd-placeholder-img card-img-top" width="100%" height="225"*/}
+                    {/*     xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"*/}
+                    {/*     preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>*/}
+                    {/*    <rect width="100%" height="100%" fill="#55595c"></rect>*/}
+                    {/*    <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>*/}
+                    {/*</svg>*/}
+
+                    <img className="card-img-top" src={this.props.sound.img} alt="Card image cap" onClick={this.props.onClick}/>
+
+                    <div className="card-body">
+                        <p className="sound-title card-text" onClick={this.props.onClick}>{this.props.sound.title}</p>
+
+                        <div className="volume-slider-container">
+                            <Slider
+                                min={0}
+                                max={100}
+                                tooltip={false}
+                                value={this.props.sound.volume}
+                                onChangeStart={this.handleChangeStart}
+                                onChange={this.handleChange}
+                                onChangeComplete={this.handleChangeComplete}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
 }
+
+export default Sound;
--- src/soundlist.jsx
+++ src/soundlist.jsx
@@ -1,5 +1,6 @@
 import sounds from './sounds.js';
 import Sound from './Sound';
+import 'react-rangeslider/lib/index.css'
 import React, {Component} from 'react';
 
 class SoundsList extends React.Component {
@@ -11,12 +12,30 @@ class SoundsList extends React.Component {
             sounds: sounds
         };
 
+        this.handleChangeVolume = this.handleChangeVolume.bind(this);
 
+
+    }
+
+    handleChangeVolume(i, volume) {
+        const sounds = this.state.sounds.slice();
+        sounds[i].sound.volume(volume / 100.0);
+        sounds[i].volume = volume;
+        this.setState({
+            playingCombinationTitle: '',
+            sounds: sounds
+        });
     }
 
     renderSounds() {
         return this.state.sounds.map((sound, index) => {
-            return <Sound/>
+            return <Sound
+                key={sound.id}
+                index={index}
+                sound={sound}
+                handleChangeVolume={this.handleChangeVolume}
+                onClick={() => this.handleClick(index)}
+            />
         })
     }
 
