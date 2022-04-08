b5b4ebf2a6d8e76ae3be4ae2e4ee4d7b4bc1c8d0
pedro <gil.catre@gmail.com>
2022-04-08 22:37:52 +0200

    change

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
                     <p className="float-end mb-1">
                         <a href="#">Back to top</a>
                     </p>
-                    <p className="mb-1">Album example is © Bootstrap, but please download and customize it for
+                    <p className="mb-1">aAlbum example is © Bootstrap, but please download and customize it for
                         yourself!</p>
                     <p className="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a
                         href="/docs/5.1/getting-started/introduction/">getting started guide</a>.</p>
