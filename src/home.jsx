258c16ef535b079c9509810d426265c180eeab29
pedro <gil.catre@gmail.com>
2022-04-11 10:28:50 +0200

    finished styling app

--- src/home.jsx
+++ src/home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
             <section className="py-5 text-center container">
                 <div className="row py-lg-5">
                     <div className="col-lg-6 col-md-8 mx-auto">
-                        <h1 className="fw-light">ASMR Sounds</h1>
+                        <h1 className="fw-light">ASMR Waves</h1>
                         <p className="lead text-muted">Mix different sounds and create your perfect environment for working, relaxing or sleeping</p>
                         {/*<p>*/}
                         {/*    <a href="#" className="btn btn-primary my-2">Main call to action</a>*/}
