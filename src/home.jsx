e7e762b9c9ba24f286a0af341ac6b21e0f20208f
pedro <gil.catre@gmail.com>
2022-04-11 09:07:43 +0200

    top section

--- src/home.jsx
+++ src/home.jsx
@@ -2,9 +2,20 @@ import SoundList from "./soundlist";
 
 export default function Home() {
     return (
-        <main style={{ padding: "1rem 0" }}>
-            <h2>Home</h2>
+        <div>
+            <section className="py-5 text-center container">
+                <div className="row py-lg-5">
+                    <div className="col-lg-6 col-md-8 mx-auto">
+                        <h1 className="fw-light">ASMR Sounds</h1>
+                        <p className="lead text-muted">Mix different sounds and create your perfect environment for working, relaxing or sleeping</p>
+                        {/*<p>*/}
+                        {/*    <a href="#" className="btn btn-primary my-2">Main call to action</a>*/}
+                        {/*    <a href="#" className="btn btn-secondary my-2">Secondary action</a>*/}
+                        {/*</p>*/}
+                    </div>
+                </div>
+            </section>
             <SoundList></SoundList>
-        </main>
+        </div>
     );
 }
