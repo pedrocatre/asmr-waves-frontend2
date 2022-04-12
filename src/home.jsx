b67ba5913fab0b09ee5d602bb43fd098716261d9
pedro <gil.catre@gmail.com>
2022-04-12 08:53:58 +0200

    added login button and modal

--- src/home.jsx
+++ src/home.jsx
@@ -1,21 +1,53 @@
 import SoundList from "./soundlist";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import React from "react";
+import classNames from 'classnames';
 
-export default function Home() {
-    return (
-        <div>
-            <section className="py-5 text-center container">
-                <div className="row py-lg-5">
-                    <div className="col-lg-6 col-md-8 mx-auto">
-                        <h1 className="fw-light">ASMR Waves</h1>
-                        <p className="lead text-muted">Mix different sounds and create your perfect environment for working, relaxing or sleeping</p>
-                        {/*<p>*/}
-                        {/*    <a href="#" className="btn btn-primary my-2">Main call to action</a>*/}
-                        {/*    <a href="#" className="btn btn-secondary my-2">Secondary action</a>*/}
-                        {/*</p>*/}
+class Home extends React.Component {
+
+    state = {
+        modal: false
+    };
+
+    toggle = () => {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <section className="py-5 text-center container">
+                    <div className="row py-lg-5">
+                        <div className="col-lg-6 col-md-8 mx-auto">
+                            <h1 className="fw-light">ASMR Waves</h1>
+                            <p className="lead text-muted">Mix different sounds and create your perfect environment for working, relaxing or sleeping</p>
+                            {/*<p>*/}
+                            {/*    <a href="#" className="btn btn-primary my-2">Main call to action</a>*/}
+                            {/*    <a href="#" className="btn btn-secondary my-2">Secondary action</a>*/}
+                            {/*</p>*/}
+                            <Button outline color="primary" onClick={this.toggle}>Log in</Button>
+
+                            <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                                <ModalHeader toggle={this.toggle}>Favorites</ModalHeader>
+                                <ModalBody>
+
+
+                                </ModalBody>
+                                <ModalFooter>
+                                    <Button color="secondary" onClick={this.toggle}>Close</Button>
+                                </ModalFooter>
+                            </Modal>
+
+                        </div>
                     </div>
-                </div>
-            </section>
-            <SoundList></SoundList>
-        </div>
-    );
+                </section>
+                <SoundList></SoundList>
+            </div>
+        );
+    }
+
 }
+
+export default Home;
