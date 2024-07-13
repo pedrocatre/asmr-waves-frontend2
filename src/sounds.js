04dcdac38274c987b6d77ccd711a43f0032e1083
pedro <gil.catre@gmail.com>
2024-07-13 11:14:06 +0200

    hair wash

--- src/sounds.js
+++ src/sounds.js
@@ -11,6 +11,7 @@ import lowThunderMp3 from './sounds/low_thunder.mp3';
 import keyboardTypingMp3 from './sounds/keyboard_typing.mp3';
 import couchMp4 from './sounds/couch_tapping.mp4';
 import handSoundsMp3 from './sounds/handsounds.mp3';
+import hairWashSoundsMp3 from './sounds/hairwash.mp3';
 import {Howl, Howler} from 'howler';
 
 // We use sprites for the songs as a trick. Just playing the sound in a loop creates a noticeable gap
@@ -108,6 +109,23 @@ export default [
                 },
                 onend: function() {
                     console.log('Finished hand sounds!');
+                }
+            }),
+            isPlaying: false,
+            volume: 100,
+            img: HandSoundImg
+        }
+        ,
+        {
+            id: 'hair-wash',
+            title: 'Hair Wash',
+            sound: new Howl({
+                src: [hairWashSoundsMp3],
+                sprite: {
+                    main: [100, 7000, true]
+                },
+                onend: function() {
+                    console.log('Finished air wash sounds!');
                 }
             }),
             isPlaying: false,
